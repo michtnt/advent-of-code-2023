237a594be9e35344f56bbb11fea3b1d6b68f627e
michtnt <michelletanotoo@gmail.com>
2023-12-04 10:51:18 +1100

    add comments day3

--- day3/index.ts
+++ day3/index.ts
@@ -1,19 +1,19 @@
 const NUMBER_REGEX = /\d+/;
 
-const getAdjacentNumbers = (nestedArr: string[], index: number) => {
+const getAdjacentNumbers = (adjacentRowArr: string[], index: number) => {
   let currentNumber = "";
   let backwardIndex = index;
   let forwardIndex = index + 1;
 
-  while (nestedArr[backwardIndex]?.match(NUMBER_REGEX)) {
-    currentNumber = nestedArr[backwardIndex].concat(currentNumber);
-    nestedArr[backwardIndex] = ".";
+  while (adjacentRowArr[backwardIndex]?.match(NUMBER_REGEX)) {
+    currentNumber = adjacentRowArr[backwardIndex].concat(currentNumber);
+    adjacentRowArr[backwardIndex] = ".";
     backwardIndex--;
   }
 
-  while (nestedArr[forwardIndex]?.match(NUMBER_REGEX)) {
-    currentNumber = currentNumber.concat(nestedArr[forwardIndex]);
-    nestedArr[forwardIndex] = ".";
+  while (adjacentRowArr[forwardIndex]?.match(NUMBER_REGEX)) {
+    currentNumber = currentNumber.concat(adjacentRowArr[forwardIndex]);
+    adjacentRowArr[forwardIndex] = ".";
     forwardIndex++;
   }
 
@@ -22,21 +22,27 @@ const getAdjacentNumbers = (nestedArr: string[], index: number) => {
 
 const getSumOfAdjacentSymbolNumbers = async () => {
   const data = await parseEngineSchematicData();
-  let totalSumOfAdjacent = 0;
-  let totalSumOfGearRatio = 0;
+
+  let totalSumOfAdjacentNumbers = 0; // part 1: any number adjacent to a symbol
+  let totalSumOfGearRatioNumbers = 0; // part 2: any * symbol that is adjacent to exactly two part numbers
 
   for (let i = 0; i < data.length; i++) {
     for (let j = 0; j < data[i].length; j++) {
+      // part 2
       let totalGearRatio = 1;
       let totalAdjacentGear = 0;
+
       const currentPosition = data[i][j];
 
       const possiblePositions = [
+        // row below
         [i - 1, j - 1],
         [i - 1, j],
         [i - 1, j + 1],
+        // row current
         [i, j - 1],
         [i, j + 1],
+        // row after
         [i + 1, j - 1],
         [i + 1, j],
         [i + 1, j + 1],
@@ -49,20 +55,24 @@ const getSumOfAdjacentSymbolNumbers = async () => {
               data[position[0]],
               position[1]
             );
-            totalSumOfAdjacent += parseInt(currentNumber);
-            totalGearRatio *= parseInt(currentNumber);
-            totalAdjacentGear++;
+
+            totalSumOfAdjacentNumbers += parseInt(currentNumber);
+
+            if (currentPosition === "*") {
+              totalGearRatio *= parseInt(currentNumber);
+              totalAdjacentGear++;
+            }
           }
         });
 
         if (totalAdjacentGear >= 2) {
-          totalSumOfGearRatio += totalGearRatio;
+          totalSumOfGearRatioNumbers += totalGearRatio;
         }
       }
     }
   }
 
-  return { totalSumOfAdjacent, totalSumOfGearRatio };
+  return { totalSumOfAdjacentNumbers, totalSumOfGearRatioNumbers };
 };
 
 /** utils */
@@ -84,13 +94,19 @@ const parseEngineSchematicData = async () => {
   return schematicMatrixArr;
 };
 
-const { totalSumOfAdjacent, totalSumOfGearRatio } =
+const start = Date.now();
+
+const { totalSumOfAdjacentNumbers, totalSumOfGearRatioNumbers } =
   await getSumOfAdjacentSymbolNumbers();
+
 console.log(
   "Part 1: sum of any adjacent numbers of symbol",
-  totalSumOfAdjacent
+  totalSumOfAdjacentNumbers
 );
 console.log(
   "Part 2: sum of two adjacent numbers multiplied of gears (*)",
-  totalSumOfGearRatio
+  totalSumOfGearRatioNumbers
 );
+
+const end = Date.now();
+console.log(`Execution time: ${end - start} ms`);
